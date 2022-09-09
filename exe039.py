2c11212b2ecf453edae99b46f1a433aa0c08ebf7
joão vitor <joaovitorrg@hotmail.com>
2022-09-09 18:30:18 -0300

    Simulado de idade para alistamento
    Recolhe os dados do candidato e confirma se ele deve se alistar naquele ano dependendo de sua idade.

---
+++ exe039.py
@@ -0,0 +1,19 @@
+from datetime import date
+ano = int(input('em qual ano você nasceu? '))
+mes = int(input('que mes?: '))
+dia = int(input('que dia?: '))
+
+def idade1(Nascimento):
+    dias_no_ano = 365.2425
+    idade = int((date.today() - Nascimento).days / dias_no_ano)
+    return idade
+print(idade1(date(ano, mes ,dia)))
+1
+atual = idade1(date(ano, mes, dia))
+diferenca = atual - 17
+if atual == 17:
+    print('Vá buscar sua enxada recruta')
+elif atual < 17:
+    print(f'Não foi dessa vez ainda faltam {diferenca*-1} anos pra você')
+else:
+    print(f'Você está acima da idade de alistamento com uma diferença de {diferenca} anos')
